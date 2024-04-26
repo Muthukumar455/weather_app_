da49e7f66d269f67b846c3dd4f6b6139be3a7299
Muthukumar455 <119417407+Muthukumar455@users.noreply.github.com>
2024-04-26 21:43:49 +0530

    Add files via upload

---
+++ weather app/weather-app/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6fd92ec61e6b306df65ea569b79ee736&units=metric`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city"
+      />
+      <button onClick={fetchWeatherData}>Get Weather</button>
+
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
